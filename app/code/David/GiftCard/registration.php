63ecc58032fcd438c668f6ecbf252620f5fd07a1
David Cao <caohuyanhtuan@gmail.com>
2020-12-08 00:04:04 +0700

    Less 01: - test commit function from Phpstorm

--- app/code/David/GiftCard/registration.php
+++ app/code/David/GiftCard/registration.php
@@ -1,12 +1,10 @@
 <?php
+/*
+ * author: David
+ * extension: GiftCard
+ * This extension is for learning
+ */
 
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'David_GiftCard', __DIR__);
-
-
-
-
-
-
-
